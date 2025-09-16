3313d0e7afb1de248a04728326f4e59eb39a8a47
timchen0618 <chen87618@gmail.com>
2025-09-16 17:26:12 -0400

    add berds eveal

--- run_eval.sh
+++ run_eval.sh
@@ -3,12 +3,82 @@
 # This script generates SBATCH files for different hyperparameter combinations
 # and submits them as separate jobs
 use_sbatch=true
-data_type="qampari+ambiguous_qe"
+data_type="qampari+ambiguous_qe" # qampari, qampari+ambiguous_qe, ambignq, berds
 save_embeddings_str="" # "--save_embeddings"
 save_before_aggregation_str="" # "--save_before_aggregation"
 suffix_list=(
-    "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPadPredLength_hungarian_contrastive_lr2e-5_temp0.05_batch16_ep60_warmup0.05_srm10"
-)
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch8_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch8_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch8_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr5e-5_temp0.05_batch8_ep120_warmup0.05_srm1"
+
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+
+    # "normalized_qampari_4gpu_full_finetuning_Fixed_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_Fixed_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_Fixed_hungarian_contrastive_lr2e-5_temp0.05_batch8_ep60_warmup0.05_srm10"
+
+    
+    # fixed model - QAMPARI  -> LoRA
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+
+    # Fixed Model - QAMPARI  -> LoRA (SRM 10)
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+
+    # fixed model - AmbigNQ  -> LoRA
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+
+    # fixed model - AmbigNQ  -> LoRA (SRM 10)
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+
+    # fixed full finetuning - AmbigNQ
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+
+    # Fixed full finetuning - QAMPARI
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+
+    # Pred Length
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPadPredLength_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch16_ep120_warmup0.05_srm1"
+    "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPadPredLength_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch16_ep120_warmup0.05_srm1"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPadPredLength_hungarian_contrastive_lr2e-5_temp0.05_batch16_ep120_warmup0.05_srm1"
+)   
+
+
 
 # Function to create SBATCH file
 create_sbatch_file() {
@@ -59,57 +129,70 @@ EOF
 
 for suffix in "${suffix_list[@]}"
 do
-    # write retrieve script
-    echo "Writing retrieve script for $suffix"
-    python write_retrieve.py $suffix  $data_type
-    sleep 1
-
-    # Load configuration
-    CONFIG_FILE="sbatch_configs/eval/retrieve_${data_type}.sh"
-    if [[ -f "$CONFIG_FILE" ]]; then
-        source "$CONFIG_FILE"
-        echo "Loaded configuration from $CONFIG_FILE"
+    if [ "$data_type" == "berds" ]; then
+        data_name_list=("arguana_generated" "kialo" "opinionqa")
     else
-        echo "Error: Configuration file $CONFIG_FILE not found!"
-        echo "Please create $CONFIG_FILE or copy from gaussian_config.sh"
-        exit 1
+        data_name_list=($data_type)
     fi
-    source sbatch_configs/eval/eval_utils.sh
-    echo "Loading utils from sbatch_configs/eval/eval_utils.sh"
 
-    # Create directories if they don't exist
-    mkdir -p "$JOB_OUTPUT_DIR"
-    mkdir -p "$SBATCH_DIR"
+    for data_name in "${data_name_list[@]}"
+    do
+        # write retrieve script
+        echo "Writing retrieve script for $suffix"
+        if [ "$data_type" == "berds" ]; then
+            python write_retrieve.py $suffix  $data_type $data_name
+        else
+            python write_retrieve.py $suffix  $data_type
+        fi
+        sleep 1
 
-    if [ "$use_sbatch" = true ]; then
-        # Create SBATCH file
-        sbatch_file=$(create_sbatch_file)
-        # Submit job 
-        job_id=$(sbatch "$sbatch_file" | awk '{print $4}')
-        echo "Submitted job with ID: $job_id"
-        echo "Job output will be in: $JOB_OUTPUT_DIR/run_${exp_name}.out"
+        # Load configuration
+        CONFIG_FILE="sbatch_configs/eval/retrieve_${data_type}.sh"
+        if [[ -f "$CONFIG_FILE" ]]; then
+            source "$CONFIG_FILE"
+            echo "Loaded configuration from $CONFIG_FILE"
+        else
+            echo "Error: Configuration file $CONFIG_FILE not found!"
+            echo "Please create $CONFIG_FILE or copy from gaussian_config.sh"
+            exit 1
+        fi
+        source sbatch_configs/eval/eval_utils.sh
+        echo "Loading utils from sbatch_configs/eval/eval_utils.sh"
 
-        echo ""
-        echo "Evaluation setup complete!"
-        echo "SBATCH files stored in: $SBATCH_DIR"
-        echo "Job outputs will be in: $JOB_OUTPUT_DIR"
-    else
-        echo "Running evaluation without sbatch"
-        python gen_ret_and_eval.py --data_name $data_name \
-        --training_data_name $training_data_name \
-        --split $split \
-        --retriever $retriever \
-        --dev_data_path $dev_data_path \
-        $gpu_str --num_shards $num_shards \
-        --base_model_id $base_model_id \
-        --adapter_path $adapter_path \
-        --linear_checkpoint_path $linear_checkpoint_path \
-        --base_model_type $base_model \
-        $max_new_tokens_str $compute_loss_str $pred_length_str --round_robin_percentage $round_robin_percentage \
-        --top_k_per_query 500 \
-        --top_k 500 \
-        --inference_modes $inference_modes \
-        --output_path $output_path \
-        $google_api $save_embeddings_str $save_before_aggregation_str
-    fi
+        # Create directories if they don't exist
+        mkdir -p "$JOB_OUTPUT_DIR"
+        mkdir -p "$SBATCH_DIR"
+
+        if [ "$use_sbatch" = true ]; then
+            # Create SBATCH file
+            sbatch_file=$(create_sbatch_file)
+            # Submit job 
+            job_id=$(sbatch "$sbatch_file" | awk '{print $4}')
+            echo "Submitted job with ID: $job_id"
+            echo "Job output will be in: $JOB_OUTPUT_DIR/run_${exp_name}.out"
+
+            echo ""
+            echo "Evaluation setup complete!"
+            echo "SBATCH files stored in: $SBATCH_DIR"
+            echo "Job outputs will be in: $JOB_OUTPUT_DIR"
+        else
+            echo "Running evaluation without sbatch"
+            python gen_ret_and_eval.py --data_name $data_name \
+            --training_data_name $training_data_name \
+            --split $split \
+            --retriever $retriever \
+            --dev_data_path $dev_data_path \
+            $gpu_str --num_shards $num_shards \
+            --base_model_id $base_model_id \
+            --adapter_path $adapter_path \
+            --linear_checkpoint_path $linear_checkpoint_path \
+            --base_model_type $base_model \
+            $max_new_tokens_str $compute_loss_str $pred_length_str --round_robin_percentage $round_robin_percentage \
+            --top_k_per_query 500 \
+            --top_k 500 \
+            --inference_modes $inference_modes \
+            --output_path $output_path \
+            $google_api $save_embeddings_str $save_before_aggregation_str
+        fi
+    done
 done
--- scripts/eval/eval_berds.sh
+++ scripts/eval/eval_berds.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 # GENERATE ARGS
-compute_only=false
+compute_only=true
 
-training_data_name="qampari"
-suffix_list="toy_qemb_from_nq"    
+training_data_name="qampari"  # ambiguous_qe, qampari
+suffix_list=(
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "mix_one_normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    # "normalized_ambiguous_qe_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr5e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+    
+    "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_hungarian_contrastive_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_ordered_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    "mix_one_normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_all_labels_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm10"
+    # "normalized_qampari_4gpu_full_finetuning_SSVariableLeftPad_contrastive_one_label_shuffled_lr2e-5_temp0.05_batch32_ep120_warmup0.05_srm1"
+
+)   
 retriever="inf"
 base_model="llama-1b"
+sanity_check_dir="fixed_model"
+
+
 
 # EVALUATE ARGS
 has_gold_id=false
@@ -37,17 +51,17 @@ else
 fi
 
 
-# for data_name in "opinionqa" "kialo" "arguana_generated"
-for data_name in "kialo"
+for data_name in "arguana_generated" "kialo" "opinionqa"
+# for data_name in "kialo"
 do
-    for suffix in $suffix_list
+    for suffix in "${suffix_list[@]}"
     do
-        ROOT="/scratch/hc3337/projects/autoregressive/results/${base_model}/${training_data_name}_${retriever}/${suffix}/"
+        ROOT="/scratch/hc3337/projects/autoregressive/results/${base_model}/${training_data_name}_${retriever}/${sanity_check_dir}/${suffix}/"
         PORT=29500
         TOPK=10
 
         # for DATA in "retrieval_out_dev_arguana_generated.jsonl.mistralpred" "retrieval_out_dev_arguana_generated_single.jsonl.mistralpred" "retrieval_out_dev_arguana_generated_from_2nd_to_3rd.jsonl.mistralpred"
-        for DATA in  "retrieval_out_dev_${data_name}.jsonl" 
+        for DATA in  "retrieval_out_dev_${data_name}_max_new_tokens_2.jsonl.mistralpred" 
         do
             MODEL_SHORT="mistral"
             MODEL_NAME="timchen0618/Mistral_BERDS_evaluator_full"
